f01609c253db7541d7f6de31192c523ee4e1d0c7
Prince Ofori <princeofori065@gmail.com>
2019-07-24 14:07:48 +0000

    Start league selection activity when get started is clicked

--- app/src/main/java/com/codejo/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/codejo/swoosh/WelcomeActivity.kt
@@ -1,12 +1,20 @@
 package com.codejo.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+        getStartedBtn.setOnClickListener {
+/* explicit intent to go to the league layout activity */
+            val leagueIntent = Intent(this, LeagueActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
