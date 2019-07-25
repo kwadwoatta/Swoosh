988b9f9c3bf1d49bf9ed89e773d08792c5c9124a
Prince Ofori <princeofori065@gmail.com>
2019-07-25 12:24:29 +0000

    Activity lifecycle logging

--- app/src/main/java/com/codejo/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/codejo/swoosh/LeagueActivity.kt
@@ -3,7 +3,7 @@ package com.codejo.swoosh
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/codejo/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/codejo/swoosh/WelcomeActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_welcome.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
