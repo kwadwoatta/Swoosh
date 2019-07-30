a8de6e27ca1f73732635f351ed4c2bed426b92b6
Prince Ofori <princeofori065@gmail.com>
2019-07-30 14:15:07 +0000

    Layout for skill selection

--- app/src/main/java/com/codejo/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/codejo/swoosh/LeagueActivity.kt
@@ -1,12 +1,19 @@
 package com.codejo.swoosh
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun leagueNextClicked(view: View) {
+        Intent(this, SkillActivity::class.java).also {
+            startActivity(it)
+        }
     }
 }
---
+++ app/src/main/java/com/codejo/swoosh/SkillActivity.kt
@@ -0,0 +1,19 @@
+package com.codejo.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class SkillActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+
+//        val skillIntent = SkillActivity
+    }
+
+    fun skillBtnClicked(view: View) {
+//        val
+    }
+}
